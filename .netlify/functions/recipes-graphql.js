1c065f96cfcf00b3ad0d70f99e7e98d2c1a5ae95
Christian Sarnataro <christian.sarnataro@vimnmix.com>
2020-05-02 16:53:53 +0200

    Added GraphQL endpoint

--- .netlify/functions/recipes-graphql.js
+++ .netlify/functions/recipes-graphql.js
@@ -1,6 +1,29 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.get('/*', function (req, res) {
   res.write(`Hello World from ${req.url}`);
