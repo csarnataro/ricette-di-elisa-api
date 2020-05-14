358d41620ab486261fbef10bb03eafefad875ab1
Christian Sarnataro <c.sarnataro@gmail.com>
2020-05-14 16:29:27 +0200

    Added some logging to  function

--- .netlify/functions/hello.js
+++ .netlify/functions/hello.js
@@ -1,8 +1,12 @@
 const fetch = require('cross-fetch');
+const qs = require('qs');
 
 const API_ENDPOINT = 'https://icanhazdadjoke.com/';
 
 exports.handler = async (event, context) => {
+  const path = event.path;
+  const apiParams = qs.stringify(event.queryStringParameters);
+  console.log(`Received request [${path}] with query ${JSON.stringify(apiParams)}`)
   return fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
     .then(response => response.json())
     .then(data => ({
